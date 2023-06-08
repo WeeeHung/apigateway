18fcab438628916a0ef51b3a2bf8bf008cb6c566
nubnubyas <zhangyiheng0421@gmail.com>
2023-06-08 18:03:09 +0800

    update

---
+++ RPC_Servers/Server3/implementation.go
@@ -0,0 +1,19 @@
+package rpcserver3
+
+import (
+	gateway_service "github.com/cloudwego/RPC_Servers/kitex_gen/gateway_service"
+	server "github.com/cloudwego/kitex/server"
+)
+
+// NewServer creates a server.Server with the given handler and options.
+func NewServer(handler gateway_service.ExampleService, opts ...server.Option) server.Server {
+	var options []server.Option
+
+	options = append(options, opts...)
+
+	svr := server.NewServer(options...)
+	if err := svr.RegisterService(serviceInfo(), handler); err != nil {
+		panic(err)
+	}
+	return svr
+}
